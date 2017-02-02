b31f66ab0c71e93031a8ad8eac29462d94f23e73
jehgong <yoimivan@gmail.com>
2017-02-02 14:18:49 -0800

    Add files via upload

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,153 +1,124 @@
-public class ArrayDeque<Item>{
-    private static double ratio = 0.25;
-    private static int HEHEXD = 2;
+public class ArrayDeque<Item> {
     private int size;
     private int nextFirst;
     private int nextLast;
-    private Item[] stuff;
-    
-    
-    public ArrayDeque(){
-        size = 0;
-        nextFirst = 4;
-        nextLast = 5;
-        stuff = (Item[]) new Object[8];
-    }
-    
-    private void resize(int cap){
-        Item[] a = (Item[]) new Object[cap];
-        int start = (cap - size) / 2;
-        if (nextFirst < nextLast){
-            int i = nextFirst + 1;
-            while(i < nextLast){
-                a[start] = stuff[i];
-                i += 1;
-            }
-        }
-        else{
-            int i = nextFirst + 1; 
-            while(i < stuff.length){
-                a[start] = stuff[i];
-                i += 1;
-            }
-            int x = 0;
-            while(x < nextLast){
-                a[start + stuff.length - nextFirst] = stuff[x];
-                x += 1;
-            }
-        }
-        nextFirst = start - 1;
-        stuff = a;
+    private Item[] items;
+
+    public ArrayDeque() {
+    int size = 0;
+    items = (Item[]) new Object[8];
+    int nextFirst = 0;
+    int nextLast = size;
     }
 
-    public boolean isEmpty(){
-        return size == 0;
+    private Item[] resize(Item[] items, boolean bool) {
+    if (bool == true) {
+        Item[] newItems = (Item[]) new Object[2*items.length];
+        System.arraycopy(items,0,newItems,0,items.length);
+        return newItems;
     }
-    
-    public int size(){
-        return size;
+    else {
+        Item[] newItems = (Item[]) new Object[items.length/2];
+        System.arraycopy(items,0,newItems,0,items.length);
+        return newItems;
+    }
+    return null;
     }
 
-    public Item get(int index){
-        if (index == 0) {
-            return stuff[0];
-        }
-        return stuff[(index+nextFirst+1)% stuff.length];
+    private void bigger(Item[] items) {
+    size = size + 1;
+    if (size == items.length) {
+        boolean bool = true;
+        items = resize(items, bool);
     }
-    
-    public void addFirst(Item x){
-        size += 1;
-        if (size >= stuff.length - 2){
-            resize(size * HEHEXD);
-        }
-        stuff[nextFirst] = x;
-        if (nextFirst == 0){
-            nextFirst = stuff.length - 1;
-        } else {
-            nextFirst = nextFirst - 1;
-        }       
     }
-    
-    public void addLast(Item x){
-        size += 1;
-        if (size >= stuff.length - 2){
-            resize(size * HEHEXD);
+
+    private void smaller(Item[] items) {
+    size = size - 1;
+    if (size <= items.length/4) {
+        if (size <= 2) {
+        return;
         }
-        stuff[nextLast] = x;
-        if (nextLast == stuff.length - 1){
-            nextLast = 0;
-        } else {
-            nextLast += 1;
+        boolean bool = false;
+        items = resize(items, bool);
+    }
+    }
+
+    public void addFirst(Item x) {
+    bigger(items);
+    items[nextFirst] = x;
+    if (nextFirst == 0) {
+        nextFirst = items.length;
+    }
+    else {
+    nextFirst = nextFirst - 1;
+    }
+    }
+
+    public void addLast(Item x) {
+        bigger(items);
+    items[nextLast] = x;
+    if (nextLast == items.length) {
+        nextLast = 0;
+    }
+    else {
+        nextLast = nextLast + 1;
+    }
+    }
+
+    public Item removeFirst() {
+    smaller(items);
+    if (nextFirst == items.length) {
+        nextFirst = 0;
+    }
+    else {
+        nextFirst = nextFirst + 1;
+    }
+    Item oldFirst = items[nextFirst];
+    items[nextFirst] = null;
+    return oldFirst;
+    }
+
+    public Item removeLast() {
+    smaller(items);
+    if (nextLast == 0) {
+        nextLast = items.length;
+    }
+    else {
+        nextLast = nextLast - 1;
+    }
+    Item oldLast = items[nextLast];
+    items[nextLast] = null;
+    return oldLast;
+    }
+
+    public Item get(int index) {
+    return items[index];
+    }
+
+    public boolean isEmpty() {
+    if (size == 0) {
+        return true;
+    }
+    return false;
+    }
+
+    public int size() {
+    return size;
+    }
+
+    public void printDeque() {
+    int index;
+    if (nextFirst == items.length) {
+        index = 0;
+    }
+    else {
+        for (index = nextFirst + 1;index <= nextLast - 1;index += 1) {
+        System.out.print(items[index]);
+        System.out.print(" ");
         }
     }
-    
-    
-    public void printDeque(){
-        if (nextFirst < nextLast){
-            int i = nextFirst + 1;
-            while (i < nextLast){
-                System.out.print(stuff[i]);
-                System.out.print(' ');
-                i += 1;
-            }
-        }
-        else{
-            int i = nextFirst + 1;
-            while(i < stuff.length){
-                System.out.print(stuff[i]);
-                System.out.print(' ');
-                i += 1;
-            }
-            int j = 0;
-            while(j < nextLast){
-                System.out.print(stuff[j]);
-                System.out.print(' ');
-                j += 1;
-            }
-        }
-        System.out.println();
     }
-    
-    public Item removeFirst(){
-        if (size == 0){
-            return null;
-        }
-        if (stuff.length >= 16 && (double)size / stuff.length <= ratio){
-            resize(size * HEHEXD);
-        }
-        Item hehe;
-        if (nextFirst == stuff.length - 1){
-            hehe = stuff[0];
-            nextFirst = 0;  
-        } else {
-            hehe = stuff[nextFirst + 1];
-            nextFirst += 1;
-        }
-        stuff[nextFirst] = null;
-        size -= 1;
-        return hehe;
-    }
-    
-    public Item removeLast(){
-        if (size == 0){
-            return null;
-        }
-        if (stuff.length >= 16 && (double)size / stuff.length <= ratio){
-            resize(size * HEHEXD);
-        }
-        Item hehe;
-        if (nextLast == 0){
-            hehe = stuff[stuff.length - 1];
-            nextLast = stuff.length - 1;
-        }
-        else{
-            hehe = stuff[nextLast - 1];
-            nextLast -= 1;
-        }
-        stuff[nextLast] = null;
-        size -= 1;
-        return hehe;
-    }
-    
-    
+        
+        
 }
