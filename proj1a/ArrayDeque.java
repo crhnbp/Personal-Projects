ef6c4f09f991653728519574ea4f48232c51277c
jehgong <yoimivan@gmail.com>
2017-02-02 15:23:43 -0800

    Add files via upload

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,124 +1,152 @@
 public class ArrayDeque<Item> {
+    private Item[] array;
     private int size;
-    private int nextFirst;
+    private Item item;
     private int nextLast;
-    private Item[] items;
+    private int nextFirst;
 
     public ArrayDeque() {
-    int size = 0;
-    items = (Item[]) new Object[8];
-    int nextFirst = 0;
-    int nextLast = size;
+        size = 0;
+        nextFirst = 0;
+        array = (Item []) new Object[8];
+        nextLast = size;
     }
 
-    private Item[] resize(Item[] items, boolean bool) {
-    if (bool == true) {
-        Item[] newItems = (Item[]) new Object[2*items.length];
-        System.arraycopy(items,0,newItems,0,items.length);
-        return newItems;
-    }
-    if (bool == false) {
-        Item[] newItems = (Item[]) new Object[items.length/2];
-        System.arraycopy(items,0,newItems,0,items.length);
-        return newItems;
-    }
-    return null;
-    }
-
-    private void bigger(Item[] items) {
-    size = size + 1;
-    if (size == items.length) {
-        boolean bool = true;
-        items = resize(items, bool);
-    }
-    }
-
-    private void smaller(Item[] items) {
-    size = size - 1;
-    if (size <= items.length/4) {
-        if (size <= 2) {
-        return;
+    private void resize(int capacity) {
+        Item[] a = (Item []) new Object[capacity];
+        if (nextFirst < nextLast) {
+            System.arraycopy(array, 0, a, 0, size);
+        } else {
+            System.arraycopy(array, nextFirst, a, 0, size - nextFirst);
+            System.arraycopy(array, 0, a, size - nextFirst, nextFirst);
         }
-        boolean bool = false;
-        items = resize(items, bool);
+        array = a;
+        nextFirst = 0;
+        nextLast = size - 1;
     }
-    }
+
 
     public void addFirst(Item x) {
-    bigger(items);
-    items[nextFirst] = x;
-    if (nextFirst == 0) {
-        nextFirst = items.length - 1;
+        if(size == 0){
+            size += 1;
+            array[nextFirst] = x;
+        } else {
+            if(size >= array.length) {
+                resize(size * 4);
+                nextFirst = array.length - 1;
+            } else {
+                if(nextFirst == 0) {
+                    nextFirst = array.length - 1;
+                } else if(nextFirst <= nextLast) {
+                    nextFirst -= 1;
+                } else {
+                    nextFirst -= 1;
+                }
+            }
+            array[nextFirst] = x;
+            size += 1;
+        }
     }
-    else {
-    nextFirst = nextFirst - 1;
-    }
-    }
+
 
     public void addLast(Item x) {
-        bigger(items);
-    items[nextLast] = x;
-    if (nextLast == items.length - 1) {
-        nextLast = 0;
-    }
-    else {
-        nextLast = nextLast + 1;
-    }
-    }
-
-    public Item removeFirst() {
-    smaller(items);
-    if (nextFirst == items.length - 1) {
-        nextFirst = 0;
-    }
-    else {
-        nextFirst = nextFirst + 1;
-    }
-    Item oldFirst = items[nextFirst];
-    items[nextFirst] = null;
-    return oldFirst;
+        if(size >= array.length){
+            resize(size * 4);
+            nextLast += 1;
+            array[nextLast] = x;
+        } else if(size == 0){
+            array[nextLast] = x;
+        } else {
+            if(nextLast == array.length - 1){
+                nextLast = 0;
+            } else {
+                nextLast += 1;
+            }
+            array[nextLast] = x;
+        }
+        size += 1;
     }
 
-    public Item removeLast() {
-    smaller(items);
-    if (nextLast == 0) {
-        nextLast = items.length - 1;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
-    else {
-        nextLast = nextLast - 1;
-    }
-    Item oldLast = items[nextLast];
-    items[nextLast] = null;
-    return oldLast;
+
+    public int size(){
+        return size;
     }
 
     public Item get(int index) {
-    return items[index];
-    }
-
-    public boolean isEmpty() {
-    if (size == 0) {
-        return true;
-    }
-    return false;
-    }
-
-    public int size() {
-    return size;
-    }
-
-    public void printDeque() {
-    int index;
-    if (nextFirst == items.length) {
-        index = 0;
-    }
-    else {
-        for (index = nextFirst + 1;index <= nextLast - 1;index += 1) {
-        System.out.print(items[index]);
-        System.out.print(" ");
+        if (size == 0) {
+            return null;
+        } else if (index == 0) {
+            return array[nextFirst];
+        } else if (nextLast < nextFirst) {
+            if (index < array.length - nextFirst) {
+                return array[index + nextFirst];
+            } else {
+                return array[index - (array.length - nextFirst)];
+            }
+        } else {
+            return array[index + nextFirst];
         }
     }
+
+
+
+    public void printDeque() {
+        if (size == 0) {
+            return;
+        }
+        if (nextLast < nextFirst) {
+            for (int i = nextFirst; i <= array.length - 1; i++) {
+                System.out.print(array[i] + " ");
+            }
+            for (int j = 0; j <= nextLast; j++) {
+                System.out.print(array[j] + " ");
+            }
+        } else {
+            for (int k = nextFirst; k <= nextLast; k++) {
+                System.out.print(array[k] + " ");
+            }
+        }
     }
-        
-        
+
+
+    public Item removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        Item Temp = array[nextFirst];
+        array[nextFirst] = null;
+        if((nextFirst < nextLast) || ((nextFirst > nextLast) && (nextFirst != array.length - 1))) {
+            nextFirst += 1;
+        } else if(nextFirst == nextLast){
+            nextFirst = nextFirst;
+        } else if(nextFirst == array.length - 1) {
+            nextFirst = 0;
+        }
+        size -= 1;
+        return Temp;
+    }
+
+
+    public Item removeLast(){
+        if(size == 0){
+            return null;
+        }
+        Item Temp = array[nextLast];
+        array[nextLast] = null;
+        if(size != 1){
+            if(nextLast == 0){
+                nextLast = array.length - 1;
+            } else {
+                nextLast -= 1;
+            }
+        }
+        size -= 1;
+        return Temp;
+    }
 }
+
+    
