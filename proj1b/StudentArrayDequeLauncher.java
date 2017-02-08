aebad70980cdfa8689fc5af65f7e05ad63aff7c6
jehgong <yoimivan@gmail.com>
2017-02-07 17:43:52 -0800

    Add files via upload

--- proj1b/StudentArrayDequeLauncher.java
+++ proj1b/StudentArrayDequeLauncher.java
@@ -3,19 +3,12 @@
 * operations */
 public class StudentArrayDequeLauncher {
     public static void main(String[] args) {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-
-        for (int i = 0; i < 10; i += 1) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-
-            if (numberBetweenZeroAndOne < 0.5) {
-                sad1.addLast(i);
-            } else {
-                sad1.addFirst(i);
-            }
-        }
-
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<Integer>();
+        sad1.addLast(5);
+        sad1.addLast(10);
         sad1.printDeque();
+    }
+} 
         /* Helpful challenge: Modify this file so that it outputs the list of
            operations as a String. Use the OperationSequence class. */
     }
