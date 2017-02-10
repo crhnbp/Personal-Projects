5a99abce6b73fde6cc8a66bb66417065a704f112
jehgong <yoimivan@gmail.com>
2017-02-09 20:05:36 -0800

    Add files via upload

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -1,69 +1,84 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.Random;
-public class TestArrayDeque1B {
+
+public class TestArrayDeque1B
+{
     @Test
-    public void testAddFirst(){
-        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+    public void test50operations()
+    {
+        StudentArrayDeque <Integer> sad1 = new StudentArrayDeque <Integer>(); 
+        ArrayDequeSolution <Integer> sol1 = new ArrayDequeSolution <Integer>();
+        int [] sequence = new int[50];
         Random rn = new Random();
-        for(int i = 0;i<10; i++){
-            int rnd = rn.nextInt();
-            student.addFirst(rnd);
-            solution.addFirst(rnd);
+        for(int n = 0; n < 50; n++)
+        {
+            int val = rn.nextInt(6);
+            sequence[n] = val;
         }
-        for(int i = 0;i<10; i++){
-            assertEquals(student.get(i), solution.get(i));
-        }
-
+        operationDriver(sequence,sad1,sol1);
     }
-    @Test
-    public void testAddLast(){
-        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
-        Random rn = new Random();
-        for(int i = 0;i<10;i++){
-            int rnd = rn.nextInt();
-            student.addLast(rnd);
-            solution.addLast(rnd);
-        }
-        for(int i =0;i<10;i++){
-            assertEquals(student.get(i), solution.get(i));
+    public void operationDriver(int [] sequence, StudentArrayDeque<Integer> sad, ArrayDequeSolution <Integer> sol)
+    {
+        Random ran = new Random();
+        String fs = "";
+        for(int i=0;i<sequence.length;i++)
+        {
+            if(sequence[i] == 0)
+            {
+                int num = ran.nextInt(50)+1;
+                sad.addFirst(num);
+                sol.addFirst(num);
+                fs = fs + "addFirst(" + num + ")" + "\n";
+                
+            }
+            if(sequence[i] == 1)
+            {
+                int num = ran.nextInt(50) + 1;
+                sad.addLast(num);
+                sol.addLast(num);
+                fs = fs + "addLast(" + num + ")" + "\n";
+            }
+            if(sequence[i] == 2)
+            {
+                fs = fs + "isEmpty()" + "\n";
+                boolean o = sol.isEmpty();
+                boolean a = sad.isEmpty();
+                assertEquals(fs,o,a);
+            }
+            if(sequence[i] == 3)
+            {
+                fs = fs + "size()" + "\n";
+                Integer o = sol.size();
+                Integer a = sad.size();
+                assertEquals(fs,o,a);
+            }
+            if(sequence[i] == 4)
+            {
+                fs = fs + "removeFirst()" + "\n";
+                Integer o = sol.removeFirst();
+                Integer a = sad.removeFirst();
+                assertEquals(fs,o,a);
+            }
+            if(sequence[i] == 5)
+            {
+                fs = fs + "removeLast()" + "\n";
+                Integer o = sol.removeLast();
+                Integer a = sad.removeLast();
+                assertEquals(fs,o,a);
+            }
+            if(sequence[i] == 6)
+            {
+                int num = ran.nextInt(sequence.length - 1);
+                fs = fs + "get(" + num + ")" + "\n";
+                Integer o = sol.get(num);
+                Integer a = sad.get(num);
+                assertEquals(fs,o,a);
+            }
         }
     }
-    @Test
-    public void testRemoveFirst(){
-        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
-        Random rn = new Random();
-        for(int i =0;i<10;i++){
-            int rnd = rn.nextInt();
-            student.addFirst(rnd);
-            solution.addFirst(rnd);
-
-        }
-        for(int i = 0;i<10; i++){
-            assertEquals(student.removeFirst(),solution.removeFirst());
-        }
-    }
-    @Test
-    public void testRemoveLast(){
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        /*test empty case*/
-        assertEquals(null, sad.removeLast());
-
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        sad1.addLast(1);
-        sad1.addLast(2);
-        sad1.addLast(3);
-        int removedItem = sad1.removeLast();
-        assertEquals(3, removedItem);
-        StudentArrayDeque<Integer> sad2 = new StudentArrayDeque<>();
-        sad2.addLast(1);
-        sad2.addLast(2);
-        sad1.printDeque();
-    }
-
-    }
-
-
+    public static void main(String[] args)
+    {
+        jh61b.junit.TestRunner.runTests("failed",TestArrayDeque1B.class);
+    }   
+}
