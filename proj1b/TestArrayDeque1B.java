d5f48392ca3e976442b45b30e908f729a570936d
jehgong <yoimivan@gmail.com>
2017-02-09 21:56:09 -0800

    Add files via upload

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -7,63 +7,63 @@ public class TestArrayDeque1B {
         jh61b.junit.TestRunner.runTests("failed", TestArrayDeque1B.class);
     }   
     @Test
-    public void testoperations() {
+    public void runner() {
         StudentArrayDeque<Integer> bad = new StudentArrayDeque<Integer>(); 
         ArrayDequeSolution<Integer> sol1 = new ArrayDequeSolution<Integer>();
-        int [] seq = new int[50];
-        Random rn = new Random();
-        for (int n = 0; n < 50; n++) {
-            int val = rn.nextInt(6);
-            seq[n] = val;
+        Random xd = new Random();
+        int[] seq = new int[50];
+        for (int n = 0; n < 50; n += 1) {
+            int value = xd.nextInt(6);
+            seq[n] = value;
         }
         ops(seq, bad, sol1);
     }
-    public void ops(int [] seq, StudentArrayDeque<Integer> sad, ArrayDequeSolution<Integer> sol) {
+    public void ops(int[] seq, StudentArrayDeque<Integer> sad, ArrayDequeSolution<Integer> sol) {
+        String str = "";
         Random ran = new Random();
-        String fs = "";
         for (int i = 0; i < seq.length; i++) {
             if (seq[i] == 0) {
-                int num = ran.nextInt(50) + 1;
+                int num = ran.nextInt(69) + 1;
                 sad.addFirst(num);
                 sol.addFirst(num);
-                fs = fs + "addFirst(" + num + ")" + "\n";
+                str = str + "addFirst(" + num + ")" + "\n";
             }
             if (seq[i] == 1) {
-                int num = ran.nextInt(50) + 1;
+                int num = ran.nextInt(69) + 1;
                 sad.addLast(num);
                 sol.addLast(num);
-                fs = fs + "addLast(" + num + ")" + "\n";
+                str = str + "addLast(" + num + ")" + "\n";
             }
             if (seq[i] == 2) {
-                fs = fs + "isEmpty()" + "\n";
+                str = str + "isEmpty()" + "\n";
                 boolean x = sol.isEmpty();
                 boolean y = sad.isEmpty();
-                assertEquals(fs, x, y);
+                assertEquals(str, x, y);
             }
             if (seq[i] == 3) {
-                fs = fs + "size()" + "\n";
+                str = str + "size()" + "\n";
                 Integer x = sol.size();
                 Integer y = sad.size();
-                assertEquals(fs, x, y);
+                assertEquals(str, x, y);
             }
             if (seq[i] == 4) {
-                fs = fs + "removeFirst()" + "\n";
+                str = str + "removeFirst()" + "\n";
                 Integer x = sol.removeFirst();
                 Integer y = sad.removeFirst();
-                assertEquals(fs, x, y);
+                assertEquals(str, x, y);
             }
             if (seq[i] == 5) {
-                fs = fs + "removeLast()" + "\n";
+                str = str + "removeLast()" + "\n";
                 Integer x = sol.removeLast();
                 Integer y = sad.removeLast();
-                assertEquals(fs, x, y);
+                assertEquals(str, x, y);
             }
             if (seq[i] == 6) {
                 int num = ran.nextInt(seq.length - 1);
-                fs = fs + "get(" + num + ")" + "\n";
+                str = str + "get(" + num + ")" + "\n";
                 Integer x = sol.get(num);
                 Integer y = sad.get(num);
-                assertEquals(fs, x, y);
+                assertEquals(str, x, y);
             }
         }
     }
