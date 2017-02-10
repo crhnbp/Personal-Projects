2587e4a87a9d23b3325a7be7eae3a1db2d46a65e
jehgong <yoimivan@gmail.com>
2017-02-09 19:08:36 -0800

    Add files via upload

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -1,86 +1,70 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
-
+import java.util.Random;
 public class TestArrayDeque1B {
-
     @Test
-    public void testSize() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<Integer>();
-        assertTrue(sad1.isEmpty());
-        assertEquals(0, sad1.size());
-
-        FailureSequence fs = new FailureSequence();
-
-        for (int i = 0; i < 10; i++) {
-            sad1.addFirst(i);
-            DequeOperation dequeOp1 = new DequeOperation("addFirst", i);
-            fs.addOperation(dequeOp1);
+    public void testAddFirst(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i = 0;i<10; i++){
+            int rnd = rn.nextInt();
+            student.addFirst(rnd);
+            solution.addFirst(rnd);
+        }
+        for(int i = 0;i<10; i++){
+            assertEquals(student.get(i), solution.get(i));
         }
 
-        assertEquals(10, sad1.size());
-
-        for (int j = 0; j < 11; j++) {
-            sad1.removeFirst();
-            DequeOperation dequeOp2 = new DequeOperation("removeFirst");
-            fs.addOperation(dequeOp2);
-        }
-
-        assertTrue(sad1.isEmpty());
-
-        DequeOperation dequeOp3 = new DequeOperation("size");
-        fs.addOperation(dequeOp3);
-
-        assertEquals(fs.toString(), 0, sad1.size());   // HURRAY! ERROR!
     }
-
     @Test
-    public void testRemoveFirst() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<Integer>();
-
-        for (int i = 0; i < 2; i++) {
-            sad1.addLast(i);
+    public void testAddLast(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i = 0;i<10;i++){
+            int rnd = rn.nextInt();
+            student.addLast(rnd);
+            solution.addLast(rnd);
         }
-
-        for (int j = 0; j < 7; j++) {
-            sad1.addFirst(j);
+        for(int i =0;i<10;i++){
+            assertEquals(student.get(i), solution.get(i));
         }
-
-        int first = sad1.removeFirst();
-        assertEquals(6, first);
     }
-
     @Test
-    public void testRemoveLast() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<Integer>();
-        for (int i = 0; i < 10; i++) {
-            sad1.addLast(i);
+    public void testRemoveFirst(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        Random rn = new Random();
+        for(int i =0;i<10;i++){
+            int rnd = rn.nextInt();
+            student.addFirst(rnd);
+            solution.addFirst(rnd);
+
         }
-
-        int last = sad1.removeLast();
-        assertEquals(9, last);
-        assertEquals(9, sad1.size());
+        for(int i = 0;i<10; i++){
+            assertEquals(student.removeFirst(),solution.removeFirst());
+        }
     }
-
     @Test
-    public void testGet() {
-        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<Integer>();
-
-        for (int i = 0; i < 2; i++) {
-            sad1.addLast(i);
+    public void testRemoveLast(){
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+        OperationSequence fs = new OperationSequence();
+        Random rn = new Random();
+        for(int i =0;i<10;i++){
+            //int rnd = rn.nextInt();
+            student.addLast(i);//instead of "i" i used "rn" for first time, "i" is easier to look at
+            solution.addLast(i);
         }
 
-        for (int j = 0; j < 7; j++) {
-            sad1.addFirst(j);
+        for(int i = 0; i < 10; i++){
+            Integer last = student.removeLast();
+            DequeOperation aa = new DequeOperation("removeLast", last);
+            fs.addOperation(aa);
+            assertEquals(fs.toString(), solution.removeLast(), last);
         }
-
-        sad1.printDeque();
-
-        int itemAtIndex = sad1.get(7);
-        assertEquals(0, itemAtIndex);
 
     }
 
-    public static void main(String... args) {
-        jh61b.junit.TestRunner.runTests("all", TestArrayDeque1B.class);
-    }
 }
