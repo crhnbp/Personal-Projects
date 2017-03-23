a02595866578c9e826478d0faeeaf60b39f5b45a
jehgong <yoimivan@gmail.com>
2017-03-23 16:31:23 -0700

    Add files via upload

--- hw3/hw3/puzzle/Solver.java
+++ hw3/hw3/puzzle/Solver.java
@@ -72,7 +72,7 @@ public class Solver {
             SearchNode current = gameTree.delMin();
             WorldState currentBoard = current.board();
             WorldState prevBoard = current.prevBoard();
-            for (Board x : currentBoard.neighbors()) {
+            for (WorldState x : currentBoard.neighbors()) {
                 if (!x.equals(prevBoard)) {
                     SearchNode newNode = new SearchNode(x, current.moves() + 1, current);
                     gameTree.insert(newNode);
@@ -100,23 +100,4 @@ public class Solver {
         return this.chosenPath;
     }
 
-    // DO NOT MODIFY MAIN METHOD
-     // Uncomment this method once your Solver and Board classes are ready.
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        int N = in.readInt();
-        int[][] tiles = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                tiles[i][j] = in.readInt();
-            }
-        }
-        Board initial = new Board(tiles);
-        Solver solver = new Solver(initial);
-        StdOut.println("Minimum number of moves = " + solver.moves());
-        for (Board board : solver.solution()) {
-            // System.out.println("printing the solution");
-            StdOut.println(board);
-        }
-    }
 }
