75e00c81481966a596eb30e360cdcd5de05894f4
jehgong <yoimivan@gmail.com>
2017-03-23 16:51:26 -0700

    Add files via upload

--- hw3/hw3/puzzle/Board.java
+++ hw3/hw3/puzzle/Board.java
@@ -28,7 +28,7 @@ public class Board implements WorldState {
                 }
             }
         }
-        hamming = ham;
+        hamming = ham - 1;
         int man = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
