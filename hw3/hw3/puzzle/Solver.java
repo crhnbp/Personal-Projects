2a868d260dc08abf537776cef5e3e8ac04401391
jehgong <yoimivan@gmail.com>
2017-03-23 15:58:48 -0700

    Add files via upload

--- hw3/hw3/puzzle/Solver.java
+++ hw3/hw3/puzzle/Solver.java
@@ -1,24 +1,24 @@
-package hw3.puzzle;
+package hw4.puzzle;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.MinPQ;
 import java.util.ArrayList;
 
 public class Solver {
-    
     private int moves;
     private MinPQ<SearchNode> gameTree;
-    private ArrayList<WorldState> chosenPath;
+    private ArrayList<Board> chosenPath;
     // private HashSet<Board> boardSaver;
     // private HashMap<Board, Integer> boardSaver;
 
     private class SearchNode implements Comparable<SearchNode> {
-        private WorldState board;
+        // The input of priority queue
+        private Board board;
         private int moves;
         private int priority;
         private SearchNode prevNode;
         
-        public SearchNode(WorldState input, int i, SearchNode prev) {
+        public SearchNode(Board input, int i, SearchNode prev) {
             board = input;
             moves = i;
             prevNode = prev;
@@ -28,6 +28,7 @@ public class Solver {
             return this.priority;
         }
 
+        // Implement the compareTo method
         public int compareTo(SearchNode other) {
             return Integer.compare(this.priority(), other.priority());
         }
@@ -47,35 +48,41 @@ public class Solver {
         public int moves() {
             return this.moves;
         }
-        public ArrayList<WorldState> chosenPath() {
+        public ArrayList<Board> chosenPath() {
             return chosenPath;
         }
     }
-
-    public Solver(WorldState initial) {
+    // Constructor
+    public Solver(Board initial) {
+        // First, insert the initial search node (the initial board, 
+        // 0 moves, and a null previous search node) into a priority queue.
         gameTree = new MinPQ<SearchNode>();
-        chosenPath = new ArrayList<WorldState>();
+        chosenPath = new ArrayList<Board>();
+        // Save the path
         chosenPath.add(0, initial);
         int moveCount = 0;
         SearchNode initialNode = new SearchNode(initial, moveCount, null);
         gameTree.insert(initialNode);
         int counter = 0;
+        // Repeat until the search node dequeued corresponds to a goal board.
         while (!gameTree.min().board().isGoal()) {
+            // Then, delete from the priority queue the search node with the minimum priority,
             int pri1 = gameTree.min().priority();
 
             SearchNode current = gameTree.delMin();
             Board currentBoard = current.board();
             Board prevBoard = current.prevBoard();
-            for (WorldState x : Board.neighbors(currentBoard)) {
+            for (Board x : BoardUtils.neighbors(currentBoard)) {
                 if (!x.equals(prevBoard)) {
                     SearchNode newNode = new SearchNode(x, current.moves() + 1, current);
                     gameTree.insert(newNode);
                 } else {
-                    counter += 1;
+                    counter++;
                 }
             }
         }
         // System.out.println(counter);
+        // Traverse back to get the path
         SearchNode end = gameTree.min();
         while (end.prevBoard() != null) {
             chosenPath.add(1, end.board());
@@ -83,13 +90,18 @@ public class Solver {
         }
     }
     public int moves() {
+        // Return the minimum number of moves to solve the initial board
         return chosenPath.size() - 1;
     }
 
-    public Iterable<WorldState> solution() {
+    public Iterable<Board> solution() {
+        // Return the sequence of Boards from the initial board to the solution
+        // Use the node to traverse back to its parent
         return this.chosenPath;
     }
 
+    // DO NOT MODIFY MAIN METHOD
+     // Uncomment this method once your Solver and Board classes are ready.
     public static void main(String[] args) {
         In in = new In(args[0]);
         int N = in.readInt();
@@ -99,7 +111,7 @@ public class Solver {
                 tiles[i][j] = in.readInt();
             }
         }
-        WorldState initial = new Board(tiles);
+        Board initial = new Board(tiles);
         Solver solver = new Solver(initial);
         StdOut.println("Minimum number of moves = " + solver.moves());
         for (Board board : solver.solution()) {
