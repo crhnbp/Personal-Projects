fd8e378fbd7517fa6b1bf2ca9fda4920186dc590
jehgong <yoimivan@gmail.com>
2017-03-23 14:33:09 -0700

    Add files via upload

--- hw3/hw3/puzzle/Board.java
+++ hw3/hw3/puzzle/Board.java
@@ -1,59 +1,58 @@
-package hw4.puzzle;
+package hw3.puzzle;
 
 // import java.lang.IndexOutOfBoundsException;
 // import java.util.Arrays;
 
 public class Board {
+    
     private final int[][] board;
     private int size;
     private int hamming;
     private int manhattan = -1;
 
-    // Constructor
     public Board(int[][] tiles) {
-        // The initial game board
         int[][] temp = tiles;
         size = tiles.length;
         board = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
                 board[i][j] = temp[i][j];
             }
         }
-        // Set hamming
-        int totalH = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        int ham = 0;
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
                 if (tileAt(i, j) != i * size + j + 1) {
-                    totalH += 1;
+                    ham += 1;
                 }
             }
         }
-        hamming = totalH;
-        // Set manhattan
-        int totalM = 0;
+        hamming = ham;
+        int man = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 int now = tileAt(i, j);
                 if (now != 0) {
                     int row = (now - 1) / size;
                     int col = (now - 1) % size;
-                    totalM += Math.abs(i - row) + Math.abs(j - col);
+                    man += Math.abs(i - row) + Math.abs(j - col);
                 }
             }
         }
-        manhattan = totalM;
+        manhattan = man;
     }
+    
     @Override
     public int hashCode() {
         int hash = java.util.Arrays.deepHashCode(this.board());
         return hash;
     }
+    
     public int[][] board() {
         return this.board;
     }
+    
     public int tileAt(int i, int j) {
-        // Return the value of tile at row i, column j 
         if (i < 0 || i > size || j < 0 || j > size) {
             throw new IndexOutOfBoundsException("index out of bounds");
         }
@@ -65,32 +64,29 @@ public class Board {
     }
 
     public int size() {
-        // Return the board size N
         return size;
     }
     public int hamming() {
-        // Hamming priority function defined above
         return this.hamming;
     }
     public int manhattan() {
         return this.manhattan;
     }
     public boolean isGoal() {
-        // Return true if is this board the goal board
         if (this == null) {
             return false;
         }
         return this.manhattan == 0;
     }
+    
     @Override
     public boolean equals(Object object) {
-        // Return true if this board's tile values are the same position as y's
         if (object == null) {
             return false;
         }
         if (this.getClass() == object.getClass()) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i += 1) {
+                for (int j = 0; j < size; j += 1) {
                     Board obj = (Board) object;
                     if (this.tileAt(i, j) != obj.tileAt(i, j)) {
                         return false;
@@ -103,14 +99,12 @@ public class Board {
         }
     }
 
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
     public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
                 s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
