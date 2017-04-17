ea4002ecb5e67e83ee5d85c038476e92221af101
jehgong <yoimivan@gmail.com>
2017-04-16 23:06:37 -0700

    Add files via upload

--- proj3/src/main/java/QuadTree.java
+++ proj3/src/main/java/QuadTree.java
@@ -1,6 +1,3 @@
-/**
- * Created by haoliangsky on 4/8/16.
- */
 import java.util.ArrayList;
 
 public class QuadTree {
@@ -84,11 +81,13 @@ public class QuadTree {
     // The x input is the root of the tree
     public ArrayList<QTreeNode> findRasterBox(QTreeNode x, double ullat, double ullon, double lrlat, double lrlon, double queryDistancePerPixel) {
         int level = 0;
+        double tileDPP = 0.00034332275390625;
         while (level <= MAX_DEPTH) {
-            double tileDPP = (this.root.getLRLON() - this.root.getULLON()) / (SIZE * Math.pow(2, level));
+
             if (tileDPP <= queryDistancePerPixel) {
                 break;
             }
+            tileDPP = tileDPP / 2;
             level += 1;
         }
         if (level > MAX_DEPTH) {
