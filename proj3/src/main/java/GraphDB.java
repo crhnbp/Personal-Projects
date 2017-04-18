e8305cecdc90a38d355a0ebd0a950c4f8f7c47a6
jehgong <yoimivan@gmail.com>
2017-04-18 14:55:15 -0700

    Add files via upload

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -1,43 +1,72 @@
 import org.xml.sax.SAXException;
+import com.sun.org.apache.xml.internal.utils.Trie;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
- * Wraps the parsing functionality of the MapDBHandler as an example.
- * You may choose to add to the functionality of this class if you wish.
- * @author Alan Yao
+ * Graph for storing all of the intersection (vertex) and road (edge) information.
+ * Uses your GraphBuildingHandler to convert the XML files into a graph. Your
+ * code must include the vertices, adjacent, distance, closest, lat, and lon
+ * methods. You'll also need to include instance variables and methods for
+ * modifying the graph (e.g. addNode and addEdge).
+ *
+ * @author Alan Yao, Josh Hug
  */
 public class GraphDB {
+    /** Your instance variables for storing the graph. You should consider
+     * creating helper classes, e.g. Node, Edge, etc. */
 
-    HashMap<Long, Node> graph;
     /**
      * Example constructor shows how to create and start an XML parser.
+     * You do not need to modify this constructor, but you're welcome to do so.
      * @param dbPath Path to the XML file to be parsed.
      */
+    public HashMap<Long, Node> graph;
+    public TrieST trieTree;
+
     public GraphDB(String dbPath) {
         graph = new HashMap<>();
         try {
             File inputFile = new File(dbPath);
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
-            MapDBHandler maphandler = new MapDBHandler(this);
-            saxParser.parse(inputFile, maphandler);
+            GraphBuildingHandler graphhandler = new GraphBuildingHandler(this);
+            saxParser.parse(inputFile, graphhandler);
+            // Get a TrieST of the nodes
+            TrieST trieTree = new TrieST(graphhandler.getNodeSet());
+
+
+
+            // Use these two sets of info to construct a graph.
+//            System.out.println(maphandler.getNodeSet().get((long) 286080658).getLat());
+            // We can do this in the connection class;
+//            System.out.println(maphandler.getWayMap().size());
+
+//            System.out.println("import size: " + maphandler.getWayMap().size());
+//            System.out.println(maphandler.getWayMap().get((long) 5149907));
+//            Connection connect = new Connection(maphandler.getNodeSet(), maphandler.getWayMap());
+//            // Then get a graph
+//            graph = connect.getGraph();
+
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
         clean();
-
     }
+
     public Map<Long, Node> getGraph() {
         return graph;
     }
+
     /**
      * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
      * @param s Input string.
@@ -45,6 +74,10 @@ public class GraphDB {
      */
     static String cleanString(String s) {
         return s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+    }
+
+    public TrieST getTrieTree() {
+        return this.trieTree;
     }
 
     /**
@@ -61,5 +94,36 @@ public class GraphDB {
                 iter.remove();
             }
         }
+    }
+
+    /** Returns an iterable of all vertex IDs in the graph. */
+    Iterable<Long> vertices() {
+        //YOUR CODE HERE, this currently returns only an empty list.
+        return new ArrayList<Long>();
+    }
+
+    /** Returns ids of all vertices adjacent to v. */
+    Iterable<Long> adjacent(long v) {
+        return null;
+    }
+
+    /** Returns the distance in units of longitude between vertices v and w. */
+    double distance(long v, long w) {
+        return 0;
+    }
+
+    /** Returns the vertex id closest to the given longitude and latitude. */
+    long closest(double lon, double lat) {
+        return 0;
+    }
+
+    /** Longitude of vertex v. */
+    double lon(long v) {
+        return 0;
+    }
+
+    /** Latitude of vertex v. */
+    double lat(long v) {
+        return 0;
     }
 }
