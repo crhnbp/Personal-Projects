25a612173691cfe5ee4fd43b6a2645c6f5f523fb
jehgong <yoimivan@gmail.com>
2017-04-17 21:13:30 -0700

    Add files via upload

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -1,36 +1,45 @@
-import com.sun.org.apache.xml.internal.utils.Trie;
 import org.xml.sax.SAXException;
+import com.sun.org.apache.xml.internal.utils.Trie;
 
 import java.io.File;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.util.ArrayList;
 import java.util.*;
 
 /**
- * Wraps the parsing functionality of the MapDBHandler as an example.
- * You may choose to add to the functionality of this class if you wish.
- * @author Alan Yao
+ * Graph for storing all of the intersection (vertex) and road (edge) information.
+ * Uses your GraphBuildingHandler to convert the XML files into a graph. Your
+ * code must include the vertices, adjacent, distance, closest, lat, and lon
+ * methods. You'll also need to include instance variables and methods for
+ * modifying the graph (e.g. addNode and addEdge).
+ *
+ * @author Alan Yao, Josh Hug
  */
 public class GraphDB {
+    /** Your instance variables for storing the graph. You should consider
+     * creating helper classes, e.g. Node, Edge, etc. */
+
     /**
      * Example constructor shows how to create and start an XML parser.
-     * @param db_path Path to the XML file to be parsed.
+     * You do not need to modify this constructor, but you're welcome to do so.
+     * @param dbPath Path to the XML file to be parsed.
      */
     public HashMap<Long, Node> graph;
     public TrieST trieTree;
 
-    public GraphDB(String db_path) {
+    public GraphDB(String dbPath) {
         graph = new HashMap<>();
         try {
             File inputFile = new File(db_path);
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
-            MapDBHandler maphandler = new MapDBHandler(this);
-            saxParser.parse(inputFile, maphandler);
+            GraphBuildingHandler graphhandler = new GraphBuildingHandler(this);
+            saxParser.parse(inputFile, graphhandler);
             // Get a TrieST of the nodes
-            TrieST trieTree = new TrieST(maphandler.getNodeSet());
+            TrieST trieTree = new TrieST(graphhandler.getNodeSet());
 
 
 
@@ -51,10 +60,6 @@ public class GraphDB {
         clean();
     }
 
-    public TrieST getTrieTree() {
-        return this.trieTree;
-    }
-
     /**
      * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
      * @param s Input string.
@@ -64,11 +69,47 @@ public class GraphDB {
         return s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
     }
 
+    public TrieST getTrieTree() {
+        return this.trieTree;
+    }
+
     /**
      *  Remove nodes with no connections from the graph.
      *  While this does not guarantee that any two nodes in the remaining graph are connected,
      *  we can reasonably assume this since typically roads are connected.
      */
     private void clean() {
+        // TODO: Your code here.
+    }
+
+    /** Returns an iterable of all vertex IDs in the graph. */
+    Iterable<Long> vertices() {
+        //YOUR CODE HERE, this currently returns only an empty list.
+        return new ArrayList<Long>();
+    }
+
+    /** Returns ids of all vertices adjacent to v. */
+    Iterable<Long> adjacent(long v) {
+        return null;
+    }
+
+    /** Returns the distance in units of longitude between vertices v and w. */
+    double distance(long v, long w) {
+        return 0;
+    }
+
+    /** Returns the vertex id closest to the given longitude and latitude. */
+    long closest(double lon, double lat) {
+        return 0;
+    }
+
+    /** Longitude of vertex v. */
+    double lon(long v) {
+        return 0;
+    }
+
+    /** Latitude of vertex v. */
+    double lat(long v) {
+        return 0;
     }
 }
