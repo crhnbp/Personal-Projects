b668ae85558b8a0ccaed3eb9c9ad627516e0780c
jehgong <yoimivan@gmail.com>
2017-04-19 21:24:19 -0700

    Add files via upload

--- proj3/src/main/java/Router.java
+++ proj3/src/main/java/Router.java
@@ -119,12 +119,14 @@ public class Router {
             }
             Set<Node> children = v.getConnectionSet();
             for (Node child: children) {
-                double edge = v.getEuclDistTo(child);
-                if (dist.get(child) >= dist.get(v) + edge || !dist.containsKey(child)) {
-                    dist.put(child, dist.get(v) + edge);                       
-                    prio.add(new WrapperNode(child, endNode, dist.get(child))); 
-                    map.put(child, v);                                    
+                if (child != null) {
+                    double edge = v.getEuclDistTo(child);
+                    if (dist.get(child) >= dist.get(v) + edge || !dist.containsKey(child)) {
+                        dist.put(child, dist.get(v) + edge);                       
+                        prio.add(new WrapperNode(child, endNode, dist.get(child))); 
+                        map.put(child, v);                                    
                 }
+            }
             }
         }
         return map;
